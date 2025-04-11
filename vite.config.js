a0fe8afba9fe24e59ae8fa225a186a8f6c1168b0
Oybek <117365256+Oybek-Rasulov@users.noreply.github.com>
2025-04-11 21:47:01 +0500

    Fix signup route and add redirects

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // ✅ Increase chunk size limit to avoid warnings
+    outDir: 'dist'
   },
+  server: {
+    port: 5173
+  }
 })
+
 
